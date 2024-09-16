1d4a3a38e4af61abd737e65d55fedbf20e4896a9
Quin Carter <quin.carter@gmail.com>
2024-09-16 13:25:51 -0400

    Updated icon to be the right url

--- src/components/header/AppShellHeader.ts
+++ src/components/header/AppShellHeader.ts
@@ -17,7 +17,7 @@ export class AppShellHeader extends LitElement {
   render(): HTMLTemplateResult {
     return html`${this.routes.length > 0
       ? html`<nav>
-            <a href="/"><img class="logo" src="${logoPng}" alt="logo" /></a>
+            <a href="home"><img class="logo" src="${logoPng}" alt="logo" /></a>
             <ul>
               ${this.routes.map(
                 route =>
